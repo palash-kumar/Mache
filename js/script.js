9ba1f4a454bf53c48a7c211e13a925062ea7abcf
palash-kumar <prince_nath@hotmail.com>
2017-06-30 03:49:01 +0600

    Check

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Prince on 25/06/2017.
+ */
+$(document).ready(function() {
+    $("[rel='tooltip']").tooltip();
+
+    $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').slideDown(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(250); //.fadeOut(205)
+        }
+    );
+});
